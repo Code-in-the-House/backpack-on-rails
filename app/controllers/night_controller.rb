1ebc9250c60f530767d7ebb6e2b8ffc9bb800896
Anıl İyidoğan <mail@aniliyidogan.com>
2016-01-09 06:37:52 +0200

    changed XML to HTML

--- app/controllers/night_controller.rb
+++ app/controllers/night_controller.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 class NightController < ApplicationController
   def index
     decathlon = open('http://www.decathlon.com.tr/C-529835-sirt-cantasi-ve-aksesuari')
-    @doc = Nokogiri::XML(decathlon)
+    @doc = Nokogiri::HTML(decathlon)
     @dec = @doc.css('ul li div.product_bloc_orga_02').first(10)
   end
 end
