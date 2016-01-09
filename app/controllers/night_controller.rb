bba7bcd02cd84673406fd09f1649ed85a8917e9e
Anıl İyidoğan <mail@aniliyidogan.com>
2016-01-09 06:17:24 +0200

    First Commit

---
+++ app/controllers/night_controller.rb
@@ -0,0 +1,8 @@
+require 'open-uri'
+class NightController < ApplicationController
+  def index
+    decathlon = open('http://www.decathlon.com.tr/C-529835-sirt-cantasi-ve-aksesuari')
+    @doc = Nokogiri::XML(decathlon)
+    @dec = @doc.css('ul li div.product_bloc_orga_02').first(10)
+  end
+end
